57bfc962e6a4f70edc84560ecd3f25520b2f1b5a
Swaroop Kondreddy <swaroopreddy.k@gmail.com>
2018-11-30 00:06:45 -0500

    comment out console.log in keys.js

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
-console.log('this is loaded');
+// console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
 };
 
 exports.omdb = {
